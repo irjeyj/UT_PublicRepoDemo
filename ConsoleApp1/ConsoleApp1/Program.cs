3fc9b29cac9f2690ad8a67fe5ec7f1011bdb46a2
irjeyj <Vijaya.KumaraS@tranetechnologies.com>
2022-11-02 16:07:26 +0530

    Added Hi IBC as disply text

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Trane Tech");
+            Console.WriteLine("Hi IBC");
             Console.ReadLine();
         }
     }
